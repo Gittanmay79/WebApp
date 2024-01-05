adf5c1c558359dccb3b4fe695efa8dc785dd1d28
Tanmay <official.tanmay79@gmail.com>
2024-01-05 23:30:33 +0530

    files added

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+const staticfolder =  express.static(path.join(__dirname, "public"));
+
+app.use(staticfolder);
+
+app.get("/", (req,res) => {res.sendFile(path.join(__dirname,"/index.html"))});
+
+app.listen(8000, () => {console.log("Server is listening at 8000")});
